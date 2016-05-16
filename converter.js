45df570475c24575f919ae2fa29ca08b419eb49c
Rui Araújo <ruka.araujo@gmail.com>
2016-05-16 16:13:17 +0200

    Convert values in messages.

--- converter.js
+++ converter.js
@@ -8,6 +8,7 @@ const args = process.argv;
 
 if (args.length < 3) {
     console.log('Usage: node converter.js folder')
+    process.exit(-1);
 }
 
 const folder = args[2];
@@ -22,5 +23,7 @@ const mergedFiles = glob.sync(path.join(folder, '**', '*.json'))
         return result;
     }, {});
 
-Object.keys(mergedFiles).forEach((locale) => fs.writeFileSync(locale, stringify(mergedFiles[locale]), 'utf-8'));
+// Filter incompatible values
+Object.keys(mergedFiles)
+    .forEach((locale) => fs.writeFileSync(locale, stringify(mergedFiles[locale]).replace(/{{/g, '{').replace(/}}/g, '}'), 'utf-8'));
 
